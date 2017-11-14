810700bc57da51285faae20bcad9d0638a96efcc
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2017-11-14 13:56:11 +0000

    oops

--- vscode/src/pclient.ts
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-import * as path from "path";
-import { workspace, ExtensionContext } from "vscode";
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient';
-
